536a0a597aaf2796a49c5b9397b859d094aaac0b
iamchapita <alejandrom646@gmail.com>
2023-07-14 16:04:06 -0600

    Cambios en sección de HomeComponents

--- tinder-frontend/src/app/pages/home/home.component.ts
+++ tinder-frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { AxiosService } from 'src/app/services/axios-service.service';
+import { Users } from 'src/app/models/user.model';
 
 @Component({
 	selector: 'app-home',
@@ -13,10 +15,35 @@ export class HomeComponent implements OnInit {
 	isUserActive: boolean = false;
 	isFireActive: boolean = true;
 	isStarActive: boolean = false;
+	users: Users[] = [];
+	user: Users;
 
-	constructor(private route: ActivatedRoute) {}
+
+	constructor(private route: ActivatedRoute, private service: AxiosService) {}
 
 	ngOnInit(): void {
 		this.id = this.route.snapshot.paramMap.get('id');
+		this.getInterestsById();
+	}
+
+	getInterestsById(): void {
+		const id = this.id ? parseInt(this.id) : null;
+
+		this.service
+			.getRegisteredUsers()
+			.then((data) => {
+				[this.user] = data.filter((user: Users) => user.id === id);
+
+				this.users = data.filter(
+					(innerUser: Users) =>
+						innerUser.id !== id &&
+						this.user.generoInteres.includes(innerUser.genero)
+				);
+
+				// console.log(this.users);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	}
 }
