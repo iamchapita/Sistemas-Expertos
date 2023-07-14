47da560869db0b722e23f1be5c55eae965a75f83
iamchapita <alejandrom646@gmail.com>
2023-07-14 17:42:25 -0600

    Implmentacion de footer y cambio de perfil renderizado

--- tinder-frontend/src/app/components/footer/footer.component.ts
+++ tinder-frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import {
+	faHeart,
+	faCircleChevronLeft,
+	faCircleChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+	faHeart = faHeart;
+	faCircleChevronLeft = faCircleChevronLeft;
+	faCircleChevronRight = faCircleChevronRight;
 
+	@Output() previousUser: EventEmitter<void> = new EventEmitter<void>();
+	@Output() nextUser: EventEmitter<void> = new EventEmitter<void>();
 }
--- tinder-frontend/src/app/pages/home/home.component.ts
+++ tinder-frontend/src/app/pages/home/home.component.ts
@@ -17,7 +17,9 @@ export class HomeComponent implements OnInit {
 	isStarActive: boolean = false;
 	users: Users[] = [];
 	user: Users;
-
+	ids: number[] = [];
+	activeUserId: number;
+	iterator: number = 0;
 
 	constructor(private route: ActivatedRoute, private service: AxiosService) {}
 
@@ -40,10 +42,36 @@ export class HomeComponent implements OnInit {
 						this.user.generoInteres.includes(innerUser.genero)
 				);
 
-				// console.log(this.users);
+				this.ids = this.users.map((user: Users) => user.id);
+				this.activeUserId = this.ids[0];
 			})
 			.catch((error) => {
 				console.log(error);
 			});
+	}
+
+	showPreviousUser(): void {
+		if (this.activeUserId > Math.min(...this.ids)) {
+			this.iterator--;
+			this.activeUserId = this.ids[this.iterator];
+		} else {
+			this.iterator = this.ids.length - 1;
+			this.activeUserId = this.ids[this.iterator];
+		}
+		console.log(this.iterator)
+
+		// console.log(this.activeUserId)
+	}
+
+	showNextUser(): void {
+		if (this.iterator < this.ids.length - 1) {
+			this.iterator++;
+			this.activeUserId = this.ids[this.iterator];
+		} else {
+			this.iterator = 0;
+			this.activeUserId = this.ids[this.iterator];
+		}
+
+		console.log(this.iterator)
 	}
 }
